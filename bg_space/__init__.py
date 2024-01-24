0959e873a91b0c11b9a057f5cf71bd6127c3f132
Will Graham <32364977+willGraham01@users.noreply.github.com>
2024-01-24 11:54:52 +0000

    Retire old name (#41)
    * Add deprecation notice to README.md
    
    * Add warning on import
    
    * Tox will ignore the deprecation warning when running tests
    
    * Hail linter, our glorious overlord
    
    * Change warning text as per Adam

--- bg_space/__init__.py
+++ bg_space/__init__.py
@@ -1,3 +1,10 @@
+from warnings import warn
+
+warn(
+    "bg-space has been renamed to brainglobe-space. To continue receiving updates, we recommend you uninstall this package and install brainglobe-space instead: https://github.com/brainglobe/brainglobe-space",
+    DeprecationWarning,
+)
+
 __author__ = """Luigi Petrucco @brainglobe"""
 __version__ = "0.6.0"
 
